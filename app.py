579371a1b6a33df3410e4075e16032280a616761
Fonzy <fonzy.fermin@gmail.com>
2025-03-28 22:46:57 +0800

    feat: add df import

--- app.py
+++ app.py
@@ -3,6 +3,14 @@ import pandas as pd
 
 app = Dash()
 
+police_killings_df = pd.read_csv("PoliceKillingsUS.csv", encoding="cp1252")
+race_share_df = pd.read_csv("ShareRaceByCity.csv", encoding="cp1252")
+med_hh_income_df = pd.read_csv("MedianHouseholdIncome2015.csv", encoding="cp1252")
+poverty_level_df = pd.read_csv(
+    "PercentagePeopleBelowPovertyLevel.csv", encoding="cp1252"
+)
+high_school_df = pd.read_csv("PercentOver25CompletedHighSchool.csv", encoding="cp1252")
+
 app.layout = [
     html.Div(children="DATA101 Project"),
 ]
